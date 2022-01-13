4cd15b0d789d36c63c1afab3a4f8262e22e66790
ChinhTQ <chinhtq@saisystem.vn>
2022-01-13 17:45:03 +0700

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,16 +1,33 @@
+import 'dart:ffi';
 import 'dart:io';
 
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:better_player/better_player.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
 
   final pathFileVideo = "".obs;
   final pathFileAudio = "".obs;
-  final RxBool play = false.obs;
+  late AssetsAudioPlayer asset;
+  final abc = "ggg".obs;
+  final playSpeed = 1.0.obs;
+  final index = 0.obs;
+  final image = <String>[
+    "https://upload.wikimedia.org/wikipedia/commons/e/e4/Elephants_Dream_cover.jpg",
+    "https://www.blendernation.com/wp-content/uploads/2008/05/afbeelding-1.png",
+    "https://i.ytimg.com/vi/TKMj7eZ_rD0/maxresdefault.jpg",
+    "https://vbcdn.com/cdn/download/2013072513747482321992901633.jpg",
+    "https://i.ytimg.com/vi/0uzxLlqtJ2c/maxresdefault.jpg"
+  ];
+  final dataSourceList = <BetterPlayerDataSource>[];
+  late BetterPlayerController betterPlayerController;
 
   @override
   void onInit() {
+    asset =  AssetsAudioPlayer();
     super.onInit();
   }
 
@@ -35,10 +52,79 @@ class HomeController extends GetxController {
     print("abc....${this.pathFileVideo}");
   }
 
-  void chooseFileAudio()async{
-    final result = await pickerFile(this.pathFileAudio.value,FileType.audio);
-    if(result == null) return;
-    this.pathFileAudio.value = result.toString();
-    print("abc....${this.pathFileAudio}");
+  void fastRewind()async{
+    await asset.seekBy(Duration(seconds: -1));
+  }
+
+  void fastForward()async{
+    await asset.seekBy(Duration(seconds: 1));
+  }
+
+  void speedUp(){
+    playSpeed.value = ++playSpeed.value;
+    asset.setPlaySpeed(playSpeed.value);
+  }
+
+  void deceleration(){
+    if(playSpeed.value != 1.0){
+      playSpeed.value = --playSpeed.value;
+      asset.setPlaySpeed(playSpeed.value);
+    }
+  }
+
+  void playAudio()async{
+    try {
+      final currentPosition = asset.currentPosition.value.toString();
+      if(!asset.isPlaying.value){
+        await asset.open(
+            Audio("assets/audios/CaCanCau.mp3"),
+            respectSilentMode: true
+        );
+        if(currentPosition != "0:00:00.000000"){
+          asset.seek(Duration(hours: int.parse(currentPosition.replaceAll(":", "").substring(0,1)),
+              minutes: int.parse(currentPosition.replaceAll(":", "").substring(1,3)),
+              seconds: int.parse(currentPosition.replaceAll(":", "").substring(3,5))));
+        }
+      }else{
+        asset.stop();
+      }
+    } catch (t) {
+      //mp3 unreachable
+    }
+  }
+
+  List<BetterPlayerDataSource> createDataSet() {
+    dataSourceList.add(
+      BetterPlayerDataSource(
+        BetterPlayerDataSourceType.network,
+        "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+      ),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(
+        BetterPlayerDataSourceType.network,
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      ),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(BetterPlayerDataSourceType.network,
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(BetterPlayerDataSourceType.network,
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(BetterPlayerDataSourceType.network,
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"),
+    );
+    return dataSourceList;
+  }
+
+  void video(int index){
+    print(dataSourceList.length);
+    betterPlayerController = BetterPlayerController(
+        BetterPlayerConfiguration(),
+        betterPlayerDataSource: dataSourceList[index]);
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:get/get.dart';
@@ -26,48 +27,115 @@ class HomeView extends GetView<HomeController> {
                 ),
               );
             }
-            return AspectRatio(
-              aspectRatio: 16 / 9,
-              child: BetterPlayer.network(
-                "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-                betterPlayerConfiguration: BetterPlayerConfiguration(
-                  aspectRatio: 16 / 9,
+            return GetX<HomeController>(builder: (_){
+              if(_.index.value == 0){
+                return AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: BetterPlayerPlaylist(
+                        betterPlayerConfiguration: BetterPlayerConfiguration(),
+                        betterPlayerPlaylistConfiguration: const BetterPlayerPlaylistConfiguration(),
+                        betterPlayerDataSourceList:  _.createDataSet())
+                );
+              }
+              return AspectRatio(
+                aspectRatio: 16 / 9,
+                child: BetterPlayer(
+                  controller: _.betterPlayerController,
                 ),
-              ),
-            );
+              );
+            });
           }),
-          TextButton(
-              onPressed:() => controller.chooseFileVideo(),
-              child: Text("chọn Video")),
-          GetX<HomeController>(builder: (_){
-            if(_.pathFileAudio.value != ""){
-              return Obx(() => AudioWidget.file(
-                path: "${_.pathFileAudio.value}",
-                play: controller.play.value,
-                child: TextButton(
-                    child: Text(
-                      controller.play.value ? "pause" : "play",
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                    onPressed: () =>
-                    controller.play.value = !controller.play.value),
-              ));
-            }
-            return Obx(() => AudioWidget.assets(
-              path: "assets/audios/CaCanCau.mp3",
-              play: controller.play.value,
-              child: TextButton(
-                  child: Text(
-                    controller.play.value ? "pause" : "play",
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  onPressed: () =>
-                  controller.play.value = !controller.play.value),
-            ));
-          }),
-          TextButton(
-              onPressed:() => controller.chooseFileAudio(),
-              child: Text("chọn Audio")),
+          CupertinoButton(
+            onPressed: () => controller.chooseFileVideo(),
+            child: Row(
+              children: [
+                Icon(Icons.add),
+                Text("chọn video")
+              ],
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                itemCount: controller.image.length,
+                  itemBuilder: (context,index){
+                  if(index == 0){
+                    return Container();
+                  }
+                    return InkWell(
+                      onTap: (){
+                        controller.index.value = index;
+                        controller.video(index);
+                      },
+                      child: Column(
+                        children: [
+                          Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Image.network(controller.image[index]),
+                              Container(
+                                padding: EdgeInsets.only(top: 10,left: 15),
+                                width: double.infinity,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  color: Colors.black38
+                                ),
+                                child: Text("Video 0${index}",style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20
+                                ),),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 16)
+                        ],
+                      ),
+                    );
+                  }
+              )
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget Audio(){
+    return Container(
+      width: 400,
+      decoration: BoxDecoration(
+          color: Colors.black26,
+          borderRadius: BorderRadius.circular(20)
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CupertinoButton(
+              child: Icon(Icons.fast_rewind),
+              onPressed: () => controller.fastRewind()),
+          CupertinoButton(
+              child: PlayerBuilder.isPlaying(
+                  player: controller.asset,
+                  builder: (context,isPlaying){
+                    return Icon(isPlaying ? Icons.pause : Icons.play_arrow);
+                  }),
+              onPressed: () => controller.playAudio()),
+          CupertinoButton(
+              child: Icon(Icons.fast_forward),
+              onPressed: () => controller.fastForward()),
+          PlayerBuilder.currentPosition(
+              player: controller.asset,
+              builder: (context, duration) {
+                return Text("${duration.toString().substring(0,7)}");
+              }
+          ),
+          SizedBox(width: Get.width/20),
+          CupertinoButton(
+              child: Icon(Icons.rotate_left_outlined),
+              onPressed: () => controller.deceleration()),
+          Obx(() => Text("${controller.playSpeed.value}x")),
+          CupertinoButton(
+              child: Icon(Icons.rotate_right),
+              onPressed: () => controller.speedUp()),
         ],
       ),
     );
